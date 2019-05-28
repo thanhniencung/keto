6b1ed6fa20c5765485c663648c8011d17967f870
MacBook <macbook@LVs-MBP-3.net.fpt>
2019-05-28 14:44:40 +0700

    refactor

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,12 +1,9 @@
-import 'item_type.dart';
-
 class Item {
   int id;
   int cateId;
   int cateName;
   String description;
-  ItemType itemType;
   double money;
 
-  Item({this.id, this.cateName, this.description, this.itemType, this.money});
+  Item({this.id, this.cateName, this.description, this.money});
 }
